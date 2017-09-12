114e954c9f453dd8629692533ba94aa5d8f1777a
sxc <shuaissss@126.com>
2017-09-13 01:12:22 +0800

    format

--- expo_components/locale-provider/en_US.js
+++
@@ -1,9 +0,0 @@
-import moment from 'moment';
-moment.locale('en');
-import Pagination from '../pagination/locale/en_US';
-import DatePicker from '../date-picker/locale/en_US';
-export default {
-    locale: 'en',
-    Pagination,
-    DatePicker,
-};
