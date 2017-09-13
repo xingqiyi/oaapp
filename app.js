a73ef9456ea95bfa1b3e7a93e21509ec675717b6
sxc <shuaissss@126.com>
2017-09-14 01:46:35 +0800

    redux

--- app.js
+++ app.js
@@ -2,9 +2,10 @@
  * @Author: shuaixc 
  * @Date: 2017-09-13 22:53:07 
  * @Last Modified by: shuaixc
- * @Last Modified time: 2017-09-14 00:17:26
+ * @Last Modified time: 2017-09-14 01:45:56
  */
 import { AppRegistry } from 'react-native';
-import Root from './src/container/app';
+// import Root from './src/containers/app';
+import Root from './src/root';
 
 AppRegistry.registerComponent('OAApp', () => Root);
