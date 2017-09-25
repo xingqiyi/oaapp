f127e3b4322997d30a690f6be0356cc97e33aa4a
sxc <shuaissss@126.com>
2017-09-26 00:37:21 +0800

    format

--- src/sagas/_spec_/category.spec.js
+++ src/sagas/_spec_/category.spec.js
@@ -24,49 +24,49 @@ import { WEXIN_ARTICLE_TYPE } from '../../constants/Urls';
 
 /* global expect */
 describe('category saga tests', () => {
-  const generator = requestTypeList();
-  const step = input => generator.next(input).value;
-  const mockTypeList = {
-    showapi_res_body: {
-      typeList: [
-        {
-          id: '19',
-          name: 'Sports'
-        },
-        {
-          id: '2',
-          name: 'Entertainment'
-        }
-      ]
-    }
-  };
+	const generator = requestTypeList();
+	const step = input => generator.next(input).value;
+	const mockTypeList = {
+		showapi_res_body: {
+			typeList: [
+				{
+					id: '19',
+					name: 'Sports'
+				},
+				{
+					id: '2',
+					name: 'Entertainment'
+				}
+			]
+		}
+	};
 
-  it('should put(fetchTypeList())', () => {
-    const next = step();
-    expect(next).toEqual(put(fetchTypeList()));
-  });
+	it('should put(fetchTypeList())', () => {
+		const next = step();
+		expect(next).toEqual(put(fetchTypeList()));
+	});
 
-  it("should call(request, WEXIN_ARTICLE_TYPE, 'get')", () => {
-    const next = step();
-    expect(next).toEqual(call(request, WEXIN_ARTICLE_TYPE, 'get'));
-  });
+	it('should call(request, WEXIN_ARTICLE_TYPE, \'get\')', () => {
+		const next = step();
+		expect(next).toEqual(call(request, WEXIN_ARTICLE_TYPE, 'get'));
+	});
 
-  it('should put(receiveTypeList(typeList.showapi_res_body.typeList))', () => {
-    const next = step(mockTypeList);
-    expect(next).toEqual(
-      put(receiveTypeList(mockTypeList.showapi_res_body.typeList))
-    );
-  });
+	it('should put(receiveTypeList(typeList.showapi_res_body.typeList))', () => {
+		const next = step(mockTypeList);
+		expect(next).toEqual(
+			put(receiveTypeList(mockTypeList.showapi_res_body.typeList))
+		);
+	});
 
-  it("should call(store.save, 'typeList', typeList.showapi_res_body.typeList)", () => {
-    const next = step(mockTypeList);
-    expect(next).toEqual(
-      call(store.save, 'typeList', mockTypeList.showapi_res_body.typeList)
-    );
-  });
+	it('should call(store.save, \'typeList\', typeList.showapi_res_body.typeList)', () => {
+		const next = step(mockTypeList);
+		expect(next).toEqual(
+			call(store.save, 'typeList', mockTypeList.showapi_res_body.typeList)
+		);
+	});
 
-  it('should be done', () => {
-    const next = generator.next();
-    expect(next.done).toEqual(true);
-  });
+	it('should be done', () => {
+		const next = generator.next();
+		expect(next.done).toEqual(true);
+	});
 });
--- src/sagas/_spec_/read.spec.js
+++ src/sagas/_spec_/read.spec.js
@@ -24,61 +24,61 @@ import { fetchArticleList, receiveArticleList } from '../../actions/read';
 
 /* global expect */
 describe('read saga tests', () => {
-  const { isRefreshing, loading, typeId, isLoadMore, page } = {
-    isRefreshing: false,
-    loading: false,
-    typeId: 2,
-    isLoadMore: false,
-    page: 1
-  };
-  const generator = requestArticleList(
-    isRefreshing,
-    loading,
-    typeId,
-    isLoadMore,
-    page
-  );
-  const mockArticleList = {
-    showapi_res_body: {
-      pagebean: {
-        contentlist: []
-      }
-    }
-  };
-  const step = input => generator.next(input).value;
+	const { isRefreshing, loading, typeId, isLoadMore, page } = {
+		isRefreshing: false,
+		loading: false,
+		typeId: 2,
+		isLoadMore: false,
+		page: 1
+	};
+	const generator = requestArticleList(
+		isRefreshing,
+		loading,
+		typeId,
+		isLoadMore,
+		page
+	);
+	const mockArticleList = {
+		showapi_res_body: {
+			pagebean: {
+				contentlist: []
+			}
+		}
+	};
+	const step = input => generator.next(input).value;
 
-  it(`should put(fetchArticleList(${isRefreshing}, ${loading}, ${isLoadMore}))`, () => {
-    const next = step();
-    expect(next).toEqual(
-      put(fetchArticleList(isRefreshing, loading, isLoadMore))
-    );
-  });
+	it(`should put(fetchArticleList(${isRefreshing}, ${loading}, ${isLoadMore}))`, () => {
+		const next = step();
+		expect(next).toEqual(
+			put(fetchArticleList(isRefreshing, loading, isLoadMore))
+		);
+	});
 
-  it(`should call(request, ${WEXIN_ARTICLE_LIST}?typeId=${typeId}&page=${page}, 'get')`, () => {
-    const next = step();
-    expect(next).toEqual(
-      call(
-        request,
-        `${WEXIN_ARTICLE_LIST}?typeId=${typeId}&page=${page}`,
-        'get'
-      )
-    );
-  });
+	it(`should call(request, ${WEXIN_ARTICLE_LIST}?typeId=${typeId}&page=${page}, 'get')`, () => {
+		const next = step();
+		expect(next).toEqual(
+			call(
+				request,
+				`${WEXIN_ARTICLE_LIST}?typeId=${typeId}&page=${page}`,
+				'get'
+			)
+		);
+	});
 
-  it(`should put(receiveArticleList(contentlist, ${typeId}))`, () => {
-    const next = step(mockArticleList);
-    expect(next).toEqual(
-      put(
-        receiveArticleList(
-          mockArticleList.showapi_res_body.pagebean.contentlist,
-          typeId
-        )
-      )
-    );
-  });
+	it(`should put(receiveArticleList(contentlist, ${typeId}))`, () => {
+		const next = step(mockArticleList);
+		expect(next).toEqual(
+			put(
+				receiveArticleList(
+					mockArticleList.showapi_res_body.pagebean.contentlist,
+					typeId
+				)
+			)
+		);
+	});
 
-  it('should be done', () => {
-    const done = generator.next().done;
-    expect(done).toEqual(true);
-  });
+	it('should be done', () => {
+		const done = generator.next().done;
+		expect(done).toEqual(true);
+	});
 });
